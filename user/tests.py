853e3d0ef0f2c151131ca63a42901bf31950cc42
tprzybylek <tajemniczyjogurt@gmail.com>
2020-02-20 19:03:47 +0100

    test: added test cases for user registration

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from user.models import CustomUser
 
-# Create your tests here.
+from django.core.cache import caches
+from rest_captcha.settings import api_settings
+from rest_captcha import utils
+
+class CaptchaTests(APITestCase):
+    def test_get_captcha(self):
+        url = reverse('rest_captcha')
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class SignUpTests(APITestCase):
+    def test_signup_correct_captcha(self):
+        url = reverse('user-signup')
+        data = {
+            'email': 'example@example.com',
+            'password': 'test123pass#@!',
+            'captcha_key': 'fe5a64f9-e0c5-4c67-9c1f-ee0a84c88af8',
+            'captcha_value': 'TEST0',
+        }
+
+        # create the key:value pair in cache for test captcha
+        cache = caches[api_settings.CAPTCHA_CACHE]
+
+        cache_key = utils.get_cache_key(data['captcha_key'])
+        cache.set(cache_key, data['captcha_value'], api_settings.CAPTCHA_TIMEOUT)
+
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_signup_wrong_captcha(self):
+        url = reverse('user-signup')
+        data = {
+            'email': 'example@example.com',
+            'password': 'test123pass#@!',
+            'captcha_key': 'fe5a64f9-e0c5-4c67-9c1f-ee0a84c88af8',
+            'captcha_value': 'wrongValue',
+        }
+
+        # create the key:value pair in cache for test captcha
+        cache = caches[api_settings.CAPTCHA_CACHE]
+        cache.set(data['captcha_key'], data['captcha_value'], api_settings.CAPTCHA_TIMEOUT)
+
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
